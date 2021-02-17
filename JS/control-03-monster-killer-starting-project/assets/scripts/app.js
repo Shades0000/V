2ed38741785fa1152a676ef7a7600f97037d9577
Shades0000 <mkucharski0000@gmail.com>
2021-02-17 19:16:07 +0100

    c

--- JS/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ JS/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,19 +3,44 @@ const BASE_PLAYER_DMG_STRONG = 17;
 const BASE_MONSTER_DMG = 14;
 const BASE_HEAL = 20;
 
-let chosenMaxLife = 100;
+let chosenMaxLife = parseInt(prompt("Enter Maximum HP"));
+
+if (isNaN(chosenMaxLife)) {
+  chosenMaxLife = 100;
+}
+console.log(chosenMaxLife);
+
 let playerHp = chosenMaxLife;
 let monsterHp = chosenMaxLife;
+let bonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  resetGame(chosenMaxLife);
+  adjustHealthBars(chosenMaxLife);
+  playerHp = chosenMaxLife;
+  monsterHp = chosenMaxLife;
+  bonusLife = true;
+}
+
 function checkHP() {
+  if (playerHp <= 0 && bonusLife) {
+    playerHp = 0.1;
+    increasePlayerHealth(0.1);
+    removeBonusLife();
+    bonusLife = false;
+    alert("Bonus Life!");
+  }
   if (monsterHp <= 0 && playerHp > 0) {
     alert("Player Wins!");
+    reset();
   } else if (playerHp <= 0 && monsterHp > 0) {
     alert("Monster Wins!");
+    reset();
   } else if (playerHp <= 0 && monsterHp <= 0) {
     alert("Draw!");
+    reset();
   }
 }
 
