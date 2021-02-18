9ea25c5e14af4330329a1d0c9ffe30e84475b714
Shades0000 <mkucharski0000@gmail.com>
2021-02-18 20:18:55 +0100

    c

--- JS/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ JS/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -2,13 +2,13 @@ const BASE_PLAYER_DMG = 10;
 const BASE_PLAYER_DMG_STRONG = 17;
 const BASE_MONSTER_DMG = 14;
 const BASE_HEAL = 20;
+let logarr = [];
 
 let chosenMaxLife = parseInt(prompt("Enter Maximum HP"));
 
 if (isNaN(chosenMaxLife)) {
   chosenMaxLife = 100;
 }
-console.log(chosenMaxLife);
 
 let playerHp = chosenMaxLife;
 let monsterHp = chosenMaxLife;
@@ -34,12 +34,17 @@ function checkHP() {
   }
   if (monsterHp <= 0 && playerHp > 0) {
     alert("Player Wins!");
+    writeToLog(playerHp, monsterHp, "Player Wins!");
+
     reset();
   } else if (playerHp <= 0 && monsterHp > 0) {
     alert("Monster Wins!");
+    writeToLog(playerHp, monsterHp, "Monster Wins!");
+
     reset();
   } else if (playerHp <= 0 && monsterHp <= 0) {
     alert("Draw!");
+    writeToLog(playerHp, monsterHp, "Draw!");
     reset();
   }
 }
@@ -67,16 +72,38 @@ function healHandler() {
   }
   playerHp += healValue;
   increasePlayerHealth(healValue);
+  writeToLog(playerHp, monsterHp, "heal");
 }
 
 function attackHandler() {
   modeHandler("BASE");
+  writeToLog(playerHp, monsterHp, "attack");
 }
 
 function strongAttackHandler() {
   modeHandler("STRONG");
+  writeToLog(playerHp, monsterHp, "strong attack");
+}
+
+function writeToLog(player_hp, monster_hp, action) {
+  let log = { player_hp: player_hp, monster_hp: monster_hp, action: action };
+  logarr.push(log);
+}
+
+function showlog() {
+  // console.log(logarr);
+
+  //  for (let index = 0; index < logarr.length; index++) {
+  //    console.log(logarr[index]);
+
+  for (const i of logarr) {
+    for (const key in i) {
+      console.log(i[key]);
+    }
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healHandler);
+logBtn.addEventListener("click", showlog);
